d1e2fe4822eba7506600b8e01a19b13cc1da747f
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-24 19:03:20 +0300

    Created a blueprint to authenticate users

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -6,7 +7,19 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    pass_secure = db.Column(db.String(255))
     
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User is {self.username}'
@@ -16,6 +29,8 @@ class Pitch(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(255))
+    # comment = db.relationship('Comment', backref = 'pitch', lazy='dynamic')
+    
 
     def __repr__(self):
         return f'Pitch is {self.title}'
