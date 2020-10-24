698d4d7372069dfad8ebf3fe2897c17e648fc9e9
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-24 17:05:31 +0300

    Created a one to many relationship between the user and Pitch class

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     '''
     General configuration parent class
     '''
-   SECRET_KEY = os.environ.get('SECRET_KEY')
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ayebale:ayebale123@localhost/pitches'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ayebale:ayebale123@localhost/pitches'
 
 
 
