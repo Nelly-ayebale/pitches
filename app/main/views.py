5ada283f2daec38db8a514418a178026c1c0e7d3
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-25 20:01:30 +0300

    Connected to Gmail

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from ..models import User, Pitch, Comment, Upvote, Downvote
-from .. import db
+from .. import db, photos
 from .forms import PitchForm, CommentForm, UpvoteForm, DownvoteForm, UpdateProfile
 from flask_login import login_required, current_user
 
@@ -32,3 +32,14 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
