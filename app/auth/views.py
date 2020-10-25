a62579aa63dae0a7437a3961d74a911cccccdb6b
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-25 16:32:37 +0300

    Created a route for registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,26 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash,request
+from flask_login import login_user, logout_user, login_required
 from . import auth
+from ..models import User
+from .forms import RegistrationForm
+from ..import db
+
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/registration', methods = ["GET", "POST"])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitches!!", "email/welcome_user", user.email, user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "NEW ACCOUNT"
+    return  render_template("auth/register.html", registration_form= form)
+    
