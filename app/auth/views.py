8263879586b3820898ebc500d8f478bef3a85fd7
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-25 17:02:16 +0300

    Formatted the navbar.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,13 +2,22 @@ from flask import render_template, redirect, url_for, flash,request
 from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
 from ..import db
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid Username or Password')
+
+    title = "Pitches || Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 @auth.route('/registration', methods = ["GET", "POST"])
 def registration():
@@ -23,4 +32,3 @@ def registration():
         return redirect(url_for('auth.login'))
         title = "NEW ACCOUNT"
     return  render_template("auth/register.html", registration_form= form)
-    
