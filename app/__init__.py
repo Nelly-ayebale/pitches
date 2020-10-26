78d853fba5489c2f661ddb606bc34160e2532154
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-26 07:33:55 +0300

    Added text to the email templates

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,8 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
-
+    
+    config_options[config_name].init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
